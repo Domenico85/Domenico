916e7b267e2ff9ff14ceca8bc85493fbba55484a
Dome <arillota@gmail.com>
2024-04-03 15:45:14 +0200

    portfolio

--- anmialShop/script.js
+++ anmialShop/script.js
@@ -1,22 +1,21 @@
-function hideBannerTemporarily(){
+function hideBanner(){
      let element = document.getElementById("overlay");
      element.classList.add("hidden");
 }
-function hideBanner() {
+function processBanner() {
 
-    let element = document.getElementById("overlay");
-    element.classList.add("hidden");
+    hideBanner();
     localStorage.setItem("bannerHidden", "true");
 }
 
-document.getElementById("consent-button").addEventListener("click", hideBanner);
-document.getElementById("exit").addEventListener("click", hideBannerTemporarily);
+document.getElementById("consent-button").addEventListener("click", processBanner);
+document.getElementById("exit").addEventListener("click", hideBanner);
 document.body.addEventListener("click", function(event){
     const userClick= event.target;
     const closeElement= document.querySelector("#overlay");
     const closeBanner= (closeElement == userClick);
     if (closeBanner){
-        hideBannerTemporarily();
+        hideBanner();
     }
     
 });
