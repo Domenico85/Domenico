fe26310ee2b40183ef2f66edbe61a53ed502755a
Dome <arillotta@gmail.com>
2024-04-19 22:40:06 +0200

    portfolio

--- SnapSphere/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    const containerWidth = document.querySelector('.container').offsetWidth;
-    const itemsContainer = document.querySelectorAll('.horizontal-scrolling-items, .horizontal-scrolling-items2');
-    itemsContainer.forEach(container => {
-        const itemsWidth = container.scrollWidth;
-        const animationDuration = (itemsWidth / containerWidth) * 10;
-        container.style.animationDuration = animationDuration + 's';
-    });
-});
