0991c4d8cb628b92aa24673364db41c012ba8322
Dome <arillotta@gmail.com>
2024-04-20 13:28:41 +0200

    portfolio

---
+++ snapScape/script.js
@@ -0,0 +1,41 @@
+function hideBanner() {
+    let element = document.getElementById("overlay");
+    element.classList.add("hidden");
+}
+function processBanner() {
+
+    hideBanner();
+    localStorage.setItem("bannerHidden", "true");
+}
+
+document.getElementById("consent-button").addEventListener("click", processBanner);
+document.getElementById("exit").addEventListener("click", hideBanner);
+document.body.addEventListener("click", function (event) {
+    const userClick = event.target;
+    const closeElement = document.querySelector("#overlay");
+    const closeBanner = (closeElement == userClick);
+    if (closeBanner) {
+        hideBanner();
+    }
+
+});
+
+window.addEventListener("load", function () {
+    let bannerHidden = localStorage.getItem("bannerHidden");
+    if (bannerHidden === "true") {
+        let element = document.getElementById("overlay");
+        element.classList.add("hidden");
+    }
+});
+
+var $ham = document.querySelector('.hamburger-container'),
+		$pageBody = document.querySelector('.page-body');
+	$ham.onclick = function () {
+		if ($ham.classList.contains('open')) {
+			$pageBody.classList.remove('open');
+			$ham.classList.remove('open');
+		} else {
+			$pageBody.classList.add('open');
+			$ham.classList.add('open');
+		}
+	};
