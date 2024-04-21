9fa88e01bd98d94f2bcac69be7a28ba2f18bf332
Dome <arillotta@gmail.com>
2024-04-21 15:28:24 +0200

    portfolio

--- Music/script.js
+++ Music/script.js
@@ -27,3 +27,25 @@ window.addEventListener("load", function(){
        element.classList.add("hidden");
    }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navToggle = document.querySelector('.main-nav-toggle');
+    var myLinks = document.querySelector('.nav-bar-text');
+
+    navToggle.addEventListener('click', function (event) {
+        event.preventDefault();
+        navToggle.classList.toggle('active-menu');
+        myLinks.classList.toggle('myLinksActive');
+    });
+
+    function showMenu() {
+        navToggle.classList.remove('active-menu');
+        myLinks.classList.remove('myLinksActive');
+    }
+
+    let links = document.querySelectorAll(".nav-bar-text a");
+
+    links.forEach(link => {
+        link.addEventListener("click", showMenu);
+    });
+});
