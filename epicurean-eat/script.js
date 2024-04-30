db1a794c916ff715ab295dff3fa2cdce90800255
Dome <arillotta@gmail.com>
2024-04-30 11:15:07 +0200

    portfolio

---
+++ epicurean-eat/script.js
@@ -0,0 +1,35 @@
+function hideBanner() {
+    let element = document.getElementById("overlay");
+    element.classList.add("hidden");
+}
+function processBanner() {
+
+    hideBanner();
+    localStorage.setItem("bannerHidden", "true");
+}
+
+document.getElementById("consent-button").addEventListener("click", processBanner);
+document.getElementById("exit").addEventListener("click", hideBanner);
+document.body.addEventListener("click", function (event) {
+    const userClick = event.target;
+    const closeElement = document.querySelector("#overlay");
+    const closeBanner = (closeElement == userClick);
+    if (closeBanner) {
+        hideBanner();
+    }
+
+});
+
+window.addEventListener("load", function () {
+    let bannerHidden = localStorage.getItem("bannerHidden");
+    if (bannerHidden === "true") {
+        let element = document.getElementById("overlay");
+        element.classList.add("hidden");
+    }
+});
+
+let nav = document.querySelector('.nav');
+document.querySelector('.menu-toggle').onclick = function(){
+  this.classList.toggle('active');
+  nav.classList.toggle('active');
+}
