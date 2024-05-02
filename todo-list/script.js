e5e12503fe7ae97ae504f2a9db4d64763b97e269
Dome <arillotta@gmail.com>
2024-05-02 12:40:53 +0200

    portfolio

--- todo-list/script.js
+++ todo-list/script.js
@@ -1,9 +1,30 @@
-function MyDay(day) {
+let newEntryForm = true
+const storedEntries = JSON.parse(localStorage.getItem('entries'));
+let entries = storedEntries || [];
 
+function saveEntries() {
+    localStorage.setItem('entries', JSON.stringify(entries));
+}
+
+function loadEntries(){
+    const storedEntries = JSON.parse(localStorage.getItem('entries'));
+    console.log(storedEntries);
+    if (storedEntries){
+        entries.length = 0;
+        storedEntries.forEach(dayData =>{
+            const day = new MyDay(dayData);
+            entries.push(day);
+            addNewDaytoDOM(day);
+        }) 
+    }
+}
+
+ window.addEventListener('load', loadEntries);
+
+function MyDay(day) {
+    this.id = Math.floor(Math.random() * 1000);
     this.title = day.title;
     this.description = day.description;
-
-    console.log(day)
     this.date = day.date;
     this.priority = day.priority;
     this.notes = day.notes;
@@ -18,6 +39,7 @@ function MyDay(day) {
     }
 }
 const form = document.querySelector('form');
+form.reset();
 form.addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -29,9 +51,24 @@ form.addEventListener('submit', function (event) {
     }
 
     const day = new MyDay(dayEntry)
-    addNewDaytoDOM(day);
+    console.log('day', day);
+    if (newEntryForm) {
+        addNewDaytoDOM(day);
+        entries.push(day);
+    } else {
+        const index = entries.findIndex(entry => entry.id === day.id);
+        if (index !== -1) {
+            Object.assign(entries[index], dayEntry);
+            editDayOnDOM(day);
+        }
+    }
+
+    console.log(entries);
+    saveEntries();
+    form.reset();
 
 })
+
 function addNewDaytoDOM(day) {
     const newDayBox = document.createElement('div');
     newDayBox.classList.add('day');
@@ -44,33 +81,29 @@ function addNewDaytoDOM(day) {
     const detailsBtn = document.createElement('button');
     detailsBtn.classList.add('details');
     detailsBtn.innerHTML = 'Details';
-    detailsBtn.addEventListener('click', function() {
+    detailsBtn.addEventListener('click', function () {
         showExampleDetails();
         createDayDetails(day);
         overlay.style.zIndex = '1';
     });
     optionsDiv.appendChild(detailsBtn);
-   
+
     const editBtn = document.createElement('button');
     editBtn.classList.add('edit');
     editBtn.innerHTML = '<img src=\"img/edit.svg"\ width=\"20px\""alt=\"edit\">';
-    editBtn.addEventListener('click', function(){
-        document.querySelector('input[name="title"]').value = day.title;
-        document.querySelector('input[name="description"]').value = day.description;
-        document.querySelector('input[name="date"]').value = day.date;
-        // document.querySelector('input[name="priority"][value="${day.priority}"]').checked = true;
-        document.querySelector('input[name="notes"]').value = day.notes;
-        // document.querySelector('input[name="checklist"][value="${day.checklist}"]').checked = true;
-
-        const form = document.querySelector('form');
-        form.style.display = 'block';
-    })  
+    editBtn.addEventListener('click', editDayOnDOM(day))
     optionsDiv.appendChild(editBtn)
-    
+
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('delete');
     deleteBtn.innerHTML = '<img src=\"img/delete.svg"\ width=\"20px\""alt=\"edit\">';
-    deleteBtn.addEventListener('click',function(){
+    deleteBtn.addEventListener('click', function () {
+        
+        entries = entries.filter(entry => entry.id !== day.id);
+            
+        saveEntries();
+        
+
         newDayBox.remove();
     });
     optionsDiv.appendChild(deleteBtn)
@@ -83,39 +116,49 @@ function createDayDetails(day) {
     const dayDetails = document.createElement('div');
     dayDetails.classList.add('details-example', 'active');
 
-    
+
     const exitButton = document.createElement('h2');
     exitButton.id = 'exit';
     exitButton.innerText = 'X';
-    exitButton.addEventListener('click', function() {
+    exitButton.addEventListener('click', function () {
         dayDetails.classList.remove('active');
-        overlay.style.zIndex = '-1'; 
+        overlay.style.zIndex = '-1';
     });
     dayDetails.appendChild(exitButton);
 
-    
+
     const title = document.createElement('h1');
     title.innerHTML = `<span>${day.title}</span>`;
     dayDetails.appendChild(title);
     const description = document.createElement('h2');
     description.innerHTML = `<span>Description<span>: ${day.description}`
     dayDetails.appendChild(description);
-    const dueDate = document.createElement ('h2');
+    const dueDate = document.createElement('h2');
     dueDate.innerHTML = `DueDate: ${day.date}`;
     dayDetails.appendChild(dueDate);
     const priority = document.createElement('h2');
     priority.innerHTML = `Priority:  ${day.priority}`
     dayDetails.appendChild(priority);
-    const notes = document.createElement ('h2');
+    const notes = document.createElement('h2');
     notes.innerHTML = `Notes: ${day.notes}`
     dayDetails.appendChild(notes);
     const checklist = document.createElement('h2');
     checklist.innerHTML = `Checklist: ${day.checklist}`;
     dayDetails.appendChild(checklist);
 
-   
+
 
     document.querySelector('#to-do-list').appendChild(dayDetails);
+}
+
+function editDayOnDOM(day) {
+    const form = document.querySelector('form');
+    form.title.value = day.title;
+    form.description.value = day.description;
+    form.date.value = day.date;
+    form.priority.value = day.priority;
+    form.notes.value = day.notes;
+
 }
 
 
@@ -168,7 +211,7 @@ function showExampleDetails() {
 showExampleDetails();
 
 function closeDetails() {
-    console.log ('close')
+    console.log('close')
     const example = document.querySelector('.details-example');
     example.classList.remove('active');
     overlay.style.zIndex = '-1'
@@ -181,8 +224,3 @@ const overlay = document.querySelector('.overlay')
 overlay.addEventListener('click', closeDetails)
 
 
-
-
-function insertTodoElement(){
-    
-}
