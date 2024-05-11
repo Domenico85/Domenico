df023a679b12c740ecee8bbfe1721511fb3999bc
Dome <arillotta@gmail.com>
2024-05-11 18:42:17 +0200

    portfolio

--- forms2/script.js
+++ forms2/script.js
@@ -1,19 +1,19 @@
-// const email = document.getElementById("mail");
-// console.log("Email input field:", email);
+function validateField(id, message) {
+  const element = document.getElementById(id);
 
-// email.addEventListener("change", (event) => {
-//   console.log("Input event triggered");
-//   if (email.validity.typeMismatch) {
-//     email.setCustomValidity("I am expecting an email address!");
-//   } else {
-//     email.setCustomValidity("");
-//   }
-// });
-
-function validateForm() {
-  let mail = document.forms["myForm"]["mail"].value;
-  if (mail == "") {
-    alert("Mail must be filled out");
-    return false;
-  }
+  element.addEventListener("focusout", (event) => {
+    if (element.validity.typeMismatch) {
+      element.setCustomValidity(message);
+    } else if (element.validity.valueMissing) {
+      element.setCustomValidity("Field Required");
+    } else if (element.validity.patternMismatch) {
+      element.setCustomValidity("Valid Number Required");
+    } else {
+      element.setCustomValidity("");
+    }
+    element.reportValidity();
+  });
 }
+
+validateField("mail", "Insert a valid e-mail");
+validateField("phone", "Insert a valid Phone Number");
