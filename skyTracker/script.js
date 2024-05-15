a8c365ee94a5aa27e8914d22f053afdc3e96c011
Dome <arillotta@gmail.com>
2024-05-15 15:12:27 +0200

    portfolio

--- skyTracker/script.js
+++ skyTracker/script.js
@@ -19,3 +19,34 @@ function hiddenPlaceholder() {
 }
 
 hiddenPlaceholder();
+
+const apiKey = "ae761484c1284fce82f91509241405";
+
+document.querySelector(".btn-search").addEventListener("click", () => {
+  const city = document.querySelector(".input-search").value.trim();
+  if (city) {
+    const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
+    console.log(url);
+
+    fetch(url, { mode: "cors" })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        document.getElementById("weather-result").innerText = JSON.stringify(
+          data,
+          null,
+          2
+        );
+      })
+      .catch((error) => {
+        console.log("There was a problem with the fetch operation:", error);
+      });
+  } else {
+    alert("Please enter a city name.");
+  }
+});
