0e8342f7e892b1a34ff82eb161082492a2b7fe81
Dome <arillotta@gmail.com>
2024-05-29 16:49:26 +0200

    portfolio

---
+++ Pruebas/hashmap/script.js
@@ -0,0 +1,16 @@
+class HashMap {
+  constructor(initialCapacity = 16) {
+    this.buckets = new Array(initialCapacity).fill(null).map(() => []);
+    this.size = 0;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
+    }
+    return hashCode;
+  }
+}
