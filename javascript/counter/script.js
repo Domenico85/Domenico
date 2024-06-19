95e520e78f5e2d07646e523f1860b91bc2b287d2
Dome <arillotta@gmail.com>
2024-06-19 20:09:22 +0200

    portfolio

---
+++ javascript/counter/script.js
@@ -0,0 +1,31 @@
+const addButtonElement = document.querySelector("#counterAdd");
+const subButtonElement = document.querySelector("#counterSub");
+const counterDisplayElement = document.querySelector("#counterDisplay");
+
+let total = 0;
+const limit = 20;
+
+const updateCounterDisplay = () => {
+  if (total > limit) {
+    total = limit;
+  }
+
+  if (total < 0) {
+    total = 0;
+  }
+  counterDisplayElement.textContent = total;
+  document.body.style.setProperty(
+    "background-color",
+    `rgb(${(total / limit) * 255}, 64, 0)`
+  );
+};
+
+addButtonElement.addEventListener("click", () => {
+  total += 1;
+  updateCounterDisplay();
+});
+
+subButtonElement.addEventListener("click", () => {
+  total -= 1;
+  updateCounterDisplay();
+});
