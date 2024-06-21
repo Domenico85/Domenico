318151a3f2a159c4efbfa5876ba2ed1c6df6a306
Dome <arillotta@gmail.com>
2024-06-21 14:59:51 +0200

    portfolio

--- javascript/testimonials/script.js
+++ javascript/testimonials/script.js
@@ -7,6 +7,30 @@ const testimonials = [
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quia, sint sit eum esse, libero, deserunt ipsum nemo recusandae architecto nesciunt. Rem labore fugiat exercitationem reiciendis est consequatur eos.",
     date: "21rd June",
   },
+  {
+    author: {
+      name: "Billy Bailey",
+      image: "img/author-02.jpg",
+    },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quia, sint sit eum esse, libero, deserunt ipsum nemo recusandae architecto nesciunt. Rem labore fugiat exercitationem reiciendis est consequatur eos.",
+    date: "25th June",
+  },
+  {
+    author: {
+      name: "Jakie Oliver",
+      image: "img/author-03.jpg",
+    },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quia, sint sit eum esse, libero, deserunt ipsum nemo recusandae architecto nesciunt. Rem labore fugiat exercitationem reiciendis est consequatur eos.",
+    date: "2nd July",
+  },
+  {
+    author: {
+      name: "Pauline Carter",
+      image: "img/author-04.jpg",
+    },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quia, sint sit eum esse, libero, deserunt ipsum nemo recusandae architecto nesciunt. Rem labore fugiat exercitationem reiciendis est consequatur eos.",
+    date: "9th July",
+  },
 ];
 
 const containerElement = document.querySelector("#testimonials-container");
@@ -21,9 +45,31 @@ const makeTestimonialCard = (testimonial) => {
     </div>`;
 };
 
-const updatePage = () => {
-  let markup = makeTestimonialCard(testimonials[0]);
+let currentTestimonial = 0;
 
+const nextTestimonial = () => {
+  if (currentTestimonial < testimonials.length - 1) {
+    currentTestimonial += 1;
+    updatePage();
+  }
+};
+
+const prevTestimonial = () => {
+  if (currentTestimonial > 0) {
+    currentTestimonial -= 1;
+    updatePage();
+  }
+};
+
+const updatePage = () => {
+  let markup = makeTestimonialCard(testimonials[currentTestimonial]);
+
+  if (testimonials.length > 1) {
+    markup += `<nav>
+        <button onclick="prevTestimonial()">Previous</button>
+        <button onclick="nextTestimonial()">Next</button>
+    </nav>`;
+  }
   containerElement.innerHTML = markup;
 };
 
