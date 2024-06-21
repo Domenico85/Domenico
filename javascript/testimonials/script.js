1c075c23900a41d223b3eb9e5aae87478b14857c
Dome <arillotta@gmail.com>
2024-06-21 14:42:46 +0200

    portfolio

--- javascript/testimonials/script.js
+++ javascript/testimonials/script.js
@@ -2,6 +2,29 @@ const testimonials = [
   {
     author: {
       name: "Gabriel Moore",
+      image: "img/author-01.jpg",
     },
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa totam quia, sint sit eum esse, libero, deserunt ipsum nemo recusandae architecto nesciunt. Rem labore fugiat exercitationem reiciendis est consequatur eos.",
+    date: "21rd June",
   },
 ];
+
+const containerElement = document.querySelector("#testimonials-container");
+
+const makeTestimonialCard = (testimonial) => {
+  return `<div class="testimonial-card">
+            <img src="${testimonial.author.image}">
+            <h2>${testimonial.author.name}</h2>
+            <p>${testimonial.text}</p>
+            <date>Written on ${testimonial.date}</date>
+    
+    </div>`;
+};
+
+const updatePage = () => {
+  let markup = makeTestimonialCard(testimonials[0]);
+
+  containerElement.innerHTML = markup;
+};
+
+updatePage();
