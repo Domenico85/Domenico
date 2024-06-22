ab1bbc99783f505b777354f268d46ffd644a5e95
Dome <arillotta@gmail.com>
2024-06-22 10:27:41 +0200

    portfolio

--- javascript/star-rating/script.js
+++ javascript/star-rating/script.js
@@ -3,6 +3,18 @@ const initialQuestions = [
     label: "Friendliness",
     rating: 0,
   },
+  {
+    label: "Cleanliness",
+    rating: 0,
+  },
+  {
+    label: "Food",
+    rating: 0,
+  },
+  {
+    label: "Service",
+    rating: 0,
+  },
 ];
 
 const storedQuestions = JSON.parse(localStorage.getItem("storedQuestions"));
@@ -30,6 +42,20 @@ const makeStars = (maxValue, question) => {
     } else {
       starElement.classList.add("empty");
     }
+
+    starElement.onclick = () => {
+      for (let i = 0; i < maxValue; i++) {
+        if (i < starPosition) {
+          starContainer.children[i].classList.add("filled");
+        } else {
+          starContainer.children[i].classList.remove("filled");
+          starContainer.children[i].classList.add("empty");
+        }
+      }
+
+      question.rating = starPosition;
+      localStorage.setItem("storedQuestions", JSON.stringify(questions));
+    };
   }
 
   return starContainer;
