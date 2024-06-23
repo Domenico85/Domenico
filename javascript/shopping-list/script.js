df5297a2a54d7134a76c870646def743cb427882
Dome <arillotta@gmail.com>
2024-06-23 11:55:11 +0200

    portfolio

--- javascript/shopping-list/script.js
+++ javascript/shopping-list/script.js
@@ -10,7 +10,19 @@ class ShoppingList {
     this.addItemButtonElement = document.querySelector(addItemButtonSelector);
     this.newItemTextElement = document.querySelector(newItemTextSelector);
 
-    this.items = JSON.parse(localStorage.getItem(this.storageKey)) || [];
+    this.items = JSON.parse(localStorage.getItem(this.storageKey)) || ["apple"];
+
+    this.initialize();
+  }
+
+  initialize() {
+    this.addItemButtonElement.addEventListener("click", () => {
+      const value = this.newItemTextElement.value;
+      this.addItem(value);
+      this.newItemTextElement.value = "";
+      this.renderItems();
+      this.storeItems();
+    });
   }
 
   renderItems() {
@@ -20,6 +32,33 @@ class ShoppingList {
       itemElement.textContent = "No Items";
       this.itemsElement.appendChild(itemElement);
     }
+
+    this.items.forEach((item, index) => {
+      const itemElement = document.createElement("li");
+      itemElement.textContent = item;
+      const removeElement = document.createElement("span");
+      removeElement.textContent = "Remove";
+      removeElement.classList.add("remove-item");
+      removeElement.onclick = () => {
+        this.removeItemAt(index);
+        this.renderItems();
+        this.storeItems();
+      };
+      itemElement.appendChild(removeElement);
+      this.itemsElement.appendChild(itemElement);
+    });
+  }
+
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+
+  removeItemAt(indexToRemove) {
+    this.items = this.items.filter((item, index) => indexToRemove != index);
+  }
+
+  storeItems() {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.items));
   }
 }
 
