92442854055bc95d4e0bd7174bdb7febab301211
Dome <arillotta@gmail.com>
2024-06-26 11:02:49 +0200

    portfolio

--- javascript/download-bar/script.js
+++ javascript/download-bar/script.js
@@ -1,1 +1,31 @@
-const fileToDownload = "wallpapers.rar";
+const fileToDownload = "wallpapers.zip";
+
+const startDownloadElement = document.querySelector("#start-download");
+
+startDownloadElement.addEventListener("click", () => {
+  console.log("Download Started");
+
+  fetch(`/${fileToDownload}`).then((response) => {
+    const reader = response.body.getReader();
+    const totalSize = Number(response.headers.get("content-length"));
+    let totalSizeDownloaded = 0;
+
+    function readData() {
+      return reader.read().then((result) => {
+        if (result.value) {
+          totalSizeDownloaded += result.value.length;
+          const percentage = Math.floor(
+            (totalSizeDownloaded / totalSize) * 100
+          );
+
+          console.log(`${totalSizeDownloaded}/${totalSize} (${percentage}%)`);
+        }
+        if (!result.done) {
+          return readData();
+        }
+      });
+    }
+
+    return readData();
+  });
+});
