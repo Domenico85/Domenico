27d485ac0629139a3061818ff1cc764e74b56031
Dome <arillotta@gmail.com>
2024-06-26 12:03:15 +0200

    portfolio

--- javascript/download-bar/script.js
+++ javascript/download-bar/script.js
@@ -8,30 +8,52 @@ const downloadProgressElement = document.querySelector(
 
 startDownloadElement.addEventListener("click", () => {
   console.log("Download Started");
+  startDownloadElement.setAttribute("disabled", "true");
+  const dataChunks = [];
 
-  fetch(`${fileToDownload}`).then((response) => {
-    const reader = response.body.getReader();
-    const totalSize = Number(response.headers.get("content-length"));
-    let totalSizeDownloaded = 0;
+  fetch(`${fileToDownload}`)
+    .then((response) => {
+      const reader = response.body.getReader();
+      const totalSize = Number(response.headers.get("content-length"));
+      let totalSizeDownloaded = 0;
 
-    function readData() {
-      return reader.read().then((result) => {
-        if (result.value) {
-          totalSizeDownloaded += result.value.length;
-          const percentage = Math.floor(
-            (totalSizeDownloaded / totalSize) * 100
-          );
+      function readData() {
+        return reader.read().then((result) => {
+          if (result.value) {
+            dataChunks.push(result.value);
+            totalSizeDownloaded += result.value.length;
+            const percentage = Math.floor(
+              (totalSizeDownloaded / totalSize) * 100
+            );
 
-          console.log(`${totalSizeDownloaded}/${totalSize} (${percentage}%)`);
+            console.log(`${totalSizeDownloaded}/${totalSize} (${percentage}%)`);
 
-          downloadProgressElement.computedStyleMap.width = `$`;
-        }
-        if (!result.done) {
-          return readData();
-        }
-      });
-    }
+            downloadProgressElement.textContent = `${percentage}%`;
+            downloadProgressElement.style.width = `${percentage}%`;
+          }
+          if (!result.done) {
+            return readData();
+          }
+        });
+      }
 
-    return readData();
-  });
+      return readData();
+    })
+    .then(() => {
+      console.log("Download finished");
+      const downloadAnchor = document.createElement("a");
+      const blob = new Blob(dataChunks);
+      downloadAnchor.href = URL.createObjectURL(blob);
+      downloadAnchor.download = fileToDownload;
+      document.body.appendChild(downloadAnchor);
+      downloadAnchor.click();
+      document.body.removeChild(downloadAnchor);
+    })
+    .catch(() => {
+      downloadProgressElement.textContent = "Download Error";
+      downloadProgressElement.classList.add("error");
+    })
+    .finally(() => {
+      startDownloadElement.removeAttribute("disabled");
+    });
 });
