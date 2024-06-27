bfc975718950fa2864cd2646a436b95337eb1aa4
Dome <arillotta@gmail.com>
2024-06-27 11:12:24 +0200

    portfolio

--- javascript/heart-trail-animation/script.js
+++ javascript/heart-trail-animation/script.js
@@ -6,5 +6,12 @@ bodyElement.addEventListener("mousemove", (event) => {
   const spanElement = document.createElement("span");
   spanElement.style.left = xPosition + "px";
   spanElement.style.top = yPosition + "px";
+  const size = Math.random() * 100;
+  spanElement.style.width = size + "px";
+  spanElement.style.height = size + "px";
+
   bodyElement.appendChild(spanElement);
+  setTimeout(() => {
+    spanElement.remove();
+  }, 3000);
 });
