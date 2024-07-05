e0b07df152f49aea196a19135ae06bc7361c5c17
Dome <arillotta@gmail.com>
2024-07-05 13:57:22 +0200

    portfolio

--- dad-joke-gen/script.js
+++ dad-joke-gen/script.js
@@ -1,4 +1,5 @@
 const btnElement = document.querySelector("#btn");
+const jokeElement = document.querySelector("#joke");
 const apiKey = "KJHiEiPTEQQVZ/NFlBhubw==NnoVSgRdanJAfN1h";
 
 const options = {
@@ -11,10 +12,25 @@ const options = {
 const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit";
 
 async function getJoke() {
-  const response = await fetch(apiURL, options);
-  const data = await response.json();
+  try {
+    jokeElement.innerText = "Updating...";
+    btnElement.disabled = true;
+    btnElement.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
 
-  console.log(data);
+    btnElement.disabled = false;
+    btnElement.innerText = "TELL ME A JOKE";
+
+    console.log(data[0].joke);
+
+    jokeElement.innerText = data[0].joke;
+  } catch (error) {
+    jokeElement.innerText = "An error happened, try again later.";
+    btnElement.disabled = false;
+    btnElement.innerText = "TELL ME A JOKE";
+    console.log(error);
+  }
 }
 
 btnElement.addEventListener("click", getJoke);
