b711f829a85948a59d0eb188bf54e6e35fd55d25
Dome <arillotta@gmail.com>
2024-07-06 14:19:59 +0200

    portfolio

---
+++ animeGatcha/script.js
@@ -0,0 +1,6 @@
+const btnElement = document.querySelector("#btn");
+const animeContainerElement = document.querySelector(".anime-container");
+const animeImgElement = document.querySelector(".anime-img");
+const animeNameElement = document.querySelector(".anime-name");
+
+const url = "https://api.nekosapi.com/v3/images/random";
