87781ae13e9a8aa2386037837ddd1088c32f265a
Dome <arillotta@gmail.com>
2024-07-17 16:01:12 +0200

    portfolio

--- furnish&flourish/script.js
+++ furnish&flourish/script.js
@@ -1,55 +1,77 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const products = {
-    0: { title: "Royal Reign", price: 525 },
-    1: { title: "White Bliss", price: 642 },
-    2: { title: "Natural Gold", price: 259 },
-    3: { title: "Aqua Leads", price: 422 },
-    4: { title: "Virgin Whites", price: 623 },
-    5: { title: "Maroon Peace", price: 295 },
-    6: { title: "Benjamin Bee", price: 458 },
-    7: { title: "Black Marvel", price: 456 },
-    8: { title: "Gold Grain", price: 509 },
-  };
+  const products = [
+    { title: "Royal Reign", price: 525 },
+    { title: "White Bliss", price: 642 },
+    { title: "Natural Gold", price: 259 },
+    { title: "Aqua Leads", price: 422 },
+    { title: "Virgin Whites", price: 623 },
+    { title: "Maroon Peace", price: 295 },
+    { title: "Benjamin Bee", price: 458 },
+    { title: "Black Marvel", price: 456 },
+    { title: "Gold Grain", price: 509 },
+  ];
 
-  localStorage.setItem("products", JSON.stringify(products));
+  let productContainer = document.querySelector(".products-container");
 
-  let productBox = document.querySelectorAll(".slideshow");
+  //   localStorage.setItem("products", JSON.stringify(products));
 
-  for (let i = 0; i < productBox.length; i++) {
-    console.log(productBox[i]);
+  // console.log(slideShow[i]);
 
-    let productTitle = JSON.parse(localStorage.getItem("products"))[i]["title"];
-    let productPrice = JSON.parse(localStorage.getItem("products"))[i]["price"];
+  // let productTitle = JSON.parse(localStorage.getItem("products"))[i]["title"];
+  // let productPrice = JSON.parse(localStorage.getItem("products"))[i]["price"];
 
-    productBox[i].querySelector(".product-title").innerHTML = productTitle;
-    productBox[i].querySelector(
-      ".box-p"
-    ).innerHTML = `Price: €${productPrice}.00`;
-    productBox[i].querySelector(
-      ".product-img"
-    ).innerHTML = `<img src="img/products/${i + 1}.jpg">`;
-  }
+  //1: calcular ancho disponible
+  //2: calcular numeros imagenes
+  //3: cuantas imgs ensenar
+  //4: aplicar translateY para index
+
+  products.forEach((product, index) => {
+    let productTitle = product.title;
+    let productPrice = product.price;
+    let productTemplate = `
+          <div class="product-list">
+            <div class="product slideshow">
+              <div class="product-img"><img src="img/products/${
+                index + 1
+              }.jpg"></div>
+              <div class="product-section-1">
+                <div class="product-title">${productTitle}</div>
+                <div class="stars"></div>
+              </div>
+              <div class="product-section-2">
+                <div class="box-p">Price:€ ${productPrice}.00</div>
+                <div>
+                  <button class="cart-btn">ADD TO CART</button>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = productTemplate;
+    productContainer.appendChild(newDiv);
+  });
 
   let num = 0;
   let screenWidth = screen.width;
   let cardsDisplayed = parseInt(screenWidth / (300 + 100));
-  let productList = document.querySelector(".slideshow");
-  let productLength = productList.length;
+  let productLength = products.length;
+  console.log(cardsDisplayed, productLength);
 
-  for (let i = num; i < num + cardsDisplayed; i++) {
-    productList[i % productLength].style.display = "flex";
-  }
+  //   for (let i = num; i < num + cardsDisplayed; i++) {
+  //     // productList[i % productLength].style.display = "flex";
+  //     console.log(i, productList);
+  //   }
 
-  function nextSlide() {
-    for (let i = num; i < num + cardsDisplayed; i++) {
-      productList[i % productLength].style.display = "none";
-    }
+  //   function nextSlide() {
+  //     for (let i = num; i < num + cardsDisplayed; i++) {
+  //       productList[i % productLength].style.display = "none";
+  //     }
 
-    num = num + cardsDisplayed;
+  //     num = num + cardsDisplayed;
 
-    for (let i = num; i < num + cardsDisplayed; i++) {
-      productList[i % productLength].style.display = "flex";
-    }
-  }
-  setInterval(nextSlide, 5000);
+  //     for (let i = num; i < num + cardsDisplayed; i++) {
+  //       productList[i % productLength].style.display = "flex";
+  //     }
+  //   }
+  //   setInterval(nextSlide, 5000);
 });
