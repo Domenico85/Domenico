1579b0642e511fe13810970676ba744a7b13105d
Dome <arillotta@gmail.com>
2024-08-11 18:17:18 +0200

    portfolio

--- tech-emporium/products.js
+++ tech-emporium/products.js
@@ -55,3 +55,41 @@ cartBtn.forEach((button) => {
     ).length;
   });
 });
+
+let slideshowButtons = document.querySelectorAll(".slideshow-buttons");
+slideshowButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    let slideshowSection = button.parentElement.dataset.slideshow;
+    console.log(slideshowSection);
+
+    let slideshowContainer = document.querySelector(
+      `#product-section-${slideshowSection}`
+    );
+    console.log(slideshowContainer);
+
+    let productCards = slideshowContainer.querySelectorAll(".product-card");
+
+    if (button.classList.contains("prev-button")) {
+      lastElement--;
+    } else if (button.classList.contains("next-button")) {
+      lastElement++;
+    } else {
+      console.log("Slideshow: Error occurred");
+    }
+
+    for (let i = 0; i < productCards.length; i++) {
+      if (i <= lastElement && i >= lastElement - (productCardPerRow - 1)) {
+        productCards[i].classList.add("active");
+
+        if (i === lastElement) {
+          productCards[i].style.marginRight = "0px";
+        } else {
+          productCards[i].style.marginRight = `${marginSpacing}px`;
+        }
+      } else {
+        productCards[i].classList.remove("active");
+        productCards[i].style.marginRight = "0px";
+      }
+    }
+  });
+});
