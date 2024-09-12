ddfab81ce75c4d7fa9fa8d054e7e6a65904fd381
Dome <arillotta@gmail.com>
2024-09-12 20:13:36 +0200

    portfolio

--- tools2/theme-clock/script.js
+++ tools2/theme-clock/script.js
@@ -4,6 +4,9 @@ const secondElement = document.querySelector(".second");
 const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
 const toggle = document.querySelector(".toggle");
+const toggleFormat = document.querySelector(".toggle-format");
+
+let is24HourFormat = false;
 
 const days = [
   "Sunday",
@@ -40,6 +43,11 @@ toggle.addEventListener("click", (e) => {
   }
 });
 
+toggleFormat.addEventListener("click", (e) => {
+  is24HourFormat = !is24HourFormat;
+  e.target.innerHTML = is24HourFormat ? "12-hour mode" : "24-hour mode";
+});
+
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
@@ -49,11 +57,15 @@ const setTime = () => {
   const date = time.getDate();
   const month = time.getMonth();
   const day = time.getDay();
-  const hours = time.getHours();
+  let hours = time.getHours();
   const hoursForClock = hours >= 13 ? hours % 12 : hours;
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
   const ampm = hours >= 12 ? "PM" : "AM";
+
+  if (!is24HourFormat) {
+    hours = hoursForClock === 0 ? 12 : hoursForClock;
+  }
 
   hourElement.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
@@ -77,9 +89,14 @@ const setTime = () => {
     360
   )}deg)`;
 
-  timeElement.innerHTML = `${hoursForClock}:${
-    minutes < 10 ? `0${minutes}` : minutes
-  } ${ampm}`;
+  timeElement.innerHTML = is24HourFormat
+    ? `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }`
+    : `${hoursForClock === 0 ? 12 : hoursForClock}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      } ${ampm}`;
+
   dateElement.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
 };
 
