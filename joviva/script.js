e9ac5d8e6f63d93ededb08c9f87f2671f11f41a4
Dome <arillotta@gmail.com>
2024-09-20 11:44:30 +0200

    portfolio

--- joviva/script.js
+++ joviva/script.js
@@ -17,3 +17,28 @@ function changeText() {
 }
 
 setInterval(changeText, 3000);
+
+const countdownDate = new Date();
+countdownDate.setDate(countdownDate.getDate() + 10);
+
+const countdownFunction = setInterval(() => {
+  const now = new Date();
+  const distance = countdownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+  if (distance < 0) {
+    clearInterval(countdownFunction);
+    document.getElementById("timer").innerHTML = "Wir sind live!";
+  }
+}, 1000);
