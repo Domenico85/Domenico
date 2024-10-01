50d10f6bca09f2a7b355cf714d517b873b4d7044
Dome <arillotta@gmail.com>
2024-10-01 13:00:14 +0200

    portfolio

--- tools3/add-to-cart/script.js
+++ tools3/add-to-cart/script.js
@@ -44,8 +44,22 @@ let ArrProducts = [
 ];
 
 const body = document.querySelector("body");
-products = document.querySelector(".products");
+let products = document.querySelector(".products");
+let shoppingBasket = document.querySelector(".shopping-basket");
+const close = document.querySelector(".close");
+let productList = document.querySelector(".product-list");
+let quantity = document.querySelector(".quantity");
+let total = document.querySelector(".total");
+
 let checkOutList = [];
+
+shoppingBasket.addEventListener("click", () => {
+  body.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  body.classList.remove("active");
+});
 
 function addItems() {
   ArrProducts.forEach((item, key) => {
@@ -53,8 +67,15 @@ function addItems() {
     let div = document.createElement("div");
     div.classList.add("item");
 
+    let star = "";
+    for (i = 0; i < item.rating; i++) {
+      //   console.log(i);
+      star += `<i class="fa-solid fa-star"></i>`;
+    }
+
     div.innerHTML = `<img src="img/${item.image}" />
     <div class="name">${item.name}</div>
+    <div class="stars">${star}
     <div class="price">${item.price}<small>€</small></div>
     <button onclick="addToCart(${key})"><i class="fa-solid fa-cart-plus"></i> Add to Cart</button>
     `;
@@ -68,5 +89,48 @@ function addToCart(Id) {
   //   console.log(ArrProducts[Id]);
   if (checkOutList[Id] == null) {
     checkOutList[Id] = ArrProducts[Id];
+    checkOutList[Id].quantity = 1;
+  } else {
+    checkOutList[Id].quantity += 1;
   }
+  reloadCart();
+}
+
+function reloadCart() {
+  productList.innerHTML = "";
+  let count = 0;
+  let totalPrice = 0;
+
+  checkOutList.forEach((item, key) => {
+    totalPrice += parseInt(item.price * item.quantity);
+    count += item.quantity;
+
+    // console.log(item);
+    let li = document.createElement("li");
+    li.innerHTML = `
+    <img src="img/${item.image}" />
+      <div>${item.name}</div>
+      <div>${item.price}</div>
+      <div>
+      <button onclick="changeQuantity(${key},${item.quantity - 1})">-</button>
+      <div class="count">${item.quantity}</div>
+      <button  onclick="changeQuantity(${key},${item.quantity + 1})">+</button>
+      </div>
+    `;
+
+    productList.appendChild(li);
+  });
+
+  total.innerHTML =
+    `<small>Subtotal <br> (${count} items)</small>€` + totalPrice;
+  quantity.innerHTML = count;
+}
+
+function changeQuantity(key, quantity) {
+  if (quantity == 0) {
+    delete checkOutList[key];
+  } else {
+    checkOutList[key].quantity = quantity;
+  }
+  reloadCart();
 }
