6e87183882ff52cf9f9c83e8b83455d410fb6571
Dome <arillotta@gmail.com>
2024-11-04 11:45:02 +0100

    portfolio

--- tools3/price-range/script.js
+++ tools3/price-range/script.js
@@ -1,33 +1,44 @@
 const rangeInputs = document.querySelectorAll(".range-input input");
-const rangeProgress = document.querySelector(".progress");
+const progressBar = document.querySelector(".progress");
 const minText = document.querySelector(".text-min");
 const maxText = document.querySelector(".text-max");
 
-let minGap = 1000;
-let maxValue = 10000;
+let minGap = 1000; // Minimum distance between min and max handles
+let maxValue = parseInt(rangeInputs[1].max); // Max value of the range
 
 rangeInputs.forEach((input) => {
-  input.addEventListener("input", updateRange);
+  input.addEventListener("input", handleRangeInput);
 });
 
-function updateRange() {
-  const minVal = parseInt(rangeInputs[0].value);
-  const maxVal = parseInt(rangeInputs[1].value);
+function handleRangeInput(e) {
+  let minVal = parseInt(rangeInputs[0].value);
+  let maxVal = parseInt(rangeInputs[1].value);
 
+  // Ensure a minimum gap between the handles
   if (maxVal - minVal < minGap) {
-    if (this.className === "range-min") {
+    if (e.target.classList.contains("range-min")) {
       rangeInputs[0].value = maxVal - minGap;
     } else {
       rangeInputs[1].value = minVal + minGap;
     }
   } else {
-    const minPercent = (minVal / maxValue) * 100;
-    const maxPercent = (maxVal / maxValue) * 100;
-
-    rangeProgress.style.left = minPercent + "%";
-    rangeProgress.style.width = maxPercent - minPercent + "%";
-
-    minText.innerText = minVal.toLocaleString();
-    maxText.innerText = maxVal.toLocaleString();
+    updateProgressBar(minVal, maxVal);
+    updateTextValues(minVal, maxVal);
   }
+}
+
+function updateProgressBar(minVal, maxVal) {
+  const minPercent = (minVal / maxValue) * 100;
+  const maxPercent = (maxVal / maxValue) * 100;
+
+  progressBar.style.left = minPercent + "%";
+  progressBar.style.width = maxPercent - minPercent + "%";
+}
+
+function updateTextValues(minVal, maxVal) {
+  minText.innerText = minVal.toLocaleString();
+  maxText.innerText = maxVal.toLocaleString();
+
+  minText.style.left = (minVal / maxValue) * 100 + "%";
+  maxText.style.right = 100 - (maxVal / maxValue) * 100 + "%";
 }
