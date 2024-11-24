e21e26e44f4f3f008d005218c34307c8667ecfa1
Dome <arillotta@gmail.com>
2024-11-24 23:59:39 +0100

    portfolio

--- react/cards/src/App.jsx
+++ react/cards/src/App.jsx
@@ -2,21 +2,43 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [message, setMessage] = useState("Domenico the fucking boss")
-  function sayMiao(value) {
-    console.log(message);
-    setMessage(value.target.value)
-    
+  const [correctAnswer, setCorrectAnswer] = useState(1);
+  const [feedback, setFeedback] = useState();
+  function isCorrectAnswer(answer){
+    return parseInt(answer) === correctAnswer;
   }
   
+  function processAnswer (event){
+    const userAnswer = parseInt(event.target.value)
+    let result = isCorrectAnswer(userAnswer)
+    console.log(result);
+    if (result == true) {
+      setFeedback("Correct")
+    }else{
+      setFeedback("Incorrect")
+    }
+    
+  
+  }
+
+
   return (
     <>
     
-      <h1> {message}</h1>
-      <input type="text" value={message} onChange={sayMiao}  />
-      <br />
-      <button onClick={sayMiao}>Click me</button>
-      
+    <h1>Choose the correct Answer</h1>
+    {feedback == "Correct" ? (
+      <h2>Right</h2>
+
+    ) : (
+      <h2>Wrong</h2>
+
+    ) }
+     
+    <div className='bottoni'>
+      <button className='button1' onClick={processAnswer} value={1}>1</button>
+      <button onClick={processAnswer} value={2}>2</button>
+      <button onClick={processAnswer} value={3}>3</button>
+      </div>
     </>
   )
 }
