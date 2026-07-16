e8e0a3aca7fe2fd6bbba1fb09de5adf85dbf96f5
Dome <arillotta@gmail.com>
2026-07-16 16:50:01 +0200

    fixed JS

--- slider-fruit-soda/script.js
+++ slider-fruit-soda/script.js
@@ -6,19 +6,31 @@ let mockup = document.querySelector(".mockup");
 
 let count = list.length;
 let active = 0;
-let leftMockup = 35;
-let left_each_item = 130 / (list.length - 1);
+
+// Calcolo dello spostamento (--left) in base alle dimensioni REALI delle immagini,
+// invece di percentuali fisse "a occhio" (che causavano lo sconfinamento tra un gusto e l'altro).
+//
+// Box della lattina (da style.css): 371px / 1.5 = 247.33px
+// listSoda.jpg è 1191x720px. Con "background-size: auto 100%" viene scalata
+// a un'altezza di 247.33px -> larghezza scalata ≈ 409.13px
+// Ogni banda di gusto (1/3 dell'immagine) scalata è quindi ≈ 136.38px
+const MOCKUP_BOX = 371 / 1.5;
+const LISTSODA_W = 1191;
+const LISTSODA_H = 720;
+
+const bgScaledWidth = LISTSODA_W * (MOCKUP_BOX / LISTSODA_H);
+const bandWidth = bgScaledWidth / count;
+const left_each_item = (bandWidth / (bgScaledWidth - MOCKUP_BOX)) * 100;
+const leftMockupStart = 0;
 
 next.addEventListener("click", () => {
   active = active >= count - 1 ? 0 : active + 1;
-  leftMockup = leftMockup + left_each_item;
   carousel.classList.remove("right");
   changeCarousel();
 });
 
 prev.addEventListener("click", () => {
   active = active <= 0 ? count - 1 : active - 1;
-  leftMockup = leftMockup - left_each_item;
   carousel.classList.add("right");
   changeCarousel();
 });
@@ -33,6 +45,7 @@ function changeCarousel() {
 
   list[active].classList.add("active");
 
+  let leftMockup = leftMockupStart + active * left_each_item;
   mockup.style.setProperty("--left", leftMockup + "%");
 
   clearInterval(refreshInterval);
